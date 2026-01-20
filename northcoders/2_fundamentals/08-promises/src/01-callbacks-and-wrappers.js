c9b8c827fdf033ac4428b5fa0cd2a2f2d1dd2d4e
vertigo1919 <149095893+vertigo1919@users.noreply.github.com>
2026-01-20 19:11:13 +0000

    Cleaned up output in 01

--- northcoders/2_fundamentals/08-promises/src/01-callbacks-and-wrappers.js
+++ northcoders/2_fundamentals/08-promises/src/01-callbacks-and-wrappers.js
@@ -1,38 +1,60 @@
+// IMPORT READFILE
+
+// in order to use readfile in the project
+// we need to assign the value of the readfile property of the object that node:fs exports
+// to a new variable called readFile > const readFile = require("node:fs").readFile;
+// But it's easier to use shorthand and write: const { readFile } = require("node:fs");
+// if we wanted to import the whole fs library we would write > const fs = require("node:fs")
+// you don't need curly brackets because you are assigning a property but the entire object
+// and then we would access readFile via fs.readFile
+
+// N.B. The fact that readFile takes a callback function doesn't have anything to do with Asynchronicity, it's just an implementation detail
+//it's the internal workings of readFile that grants its asynchronous execution, meaning that the main JS thread execution can continue
+
 const { readFile } = require("node:fs");
 
-// in order to import readfile we need to assign the value of the readfile property of the object that node:fs exports to a new variable called readFile
-// so we could write: const readFile = require("node:fs").readFile;
-// instead we use shorthand and we write: const { readFile } = require("node:fs");
-// if we wanted to import the whole fs library we would write
-// const fs = require("node:fs") (you don't need curly brackets because you are assigning a property but the entire object - and then we could access readFile via:
-// fs.readFile
+//##TASK 1: read a file from the disk without "blocking" the rest of the program
+const task = "TASK 1 >";
+const errorX = "❌ Error:";
+const success = "✅ Success!";
 
-// N.B. The fact that readFile takes a callback function doesn't have to do with Asynchronicity, it's just an implementation detail
-//it's the internal workings of readFile that grants it asynchronous execution meaning that the main JS thread execution will continue
+console.log(task, "Requesting file read...");
 
-// here we grab the promisified version
-
-//##TASK 1
 readFile("./data/03_poem.txt", "utf-8", (err, data) => {
   if (err) {
-    console.log("TASK1> ", err);
+    console.log(task, errorX, err.message);
   } else {
-    console.log("TASK1> ", data);
+    console.log(task, success, "Content length:", data.length, "characters");
+    console.log(task, "Content Preview:", data.slice(0, 30) + "...");
   }
 });
 
-//TASK 1 - EXTENSION
-// Use what we've written to write a new function readAndThen() which executes readFile() and, only once that's done, it performs a second step
-// To do it with callbacks we need to  wrap  readFile() within a new function readAndThen() and make it take as a parameter a callback function that is invoked only after the value.
+console.log(
+  task,
+  "This log runs BEFORE the file is finished reading as the reading is asynchronous!"
+);
+
+//##TASK 1 EXTENSION > create a wrapper function to define a "second step"
+// I now want to use what we've written to write a new function readAndThen() which executes readFile() and, only once that's done, it performs a second step
+// To do it with callbacks we need to  wrap readFile() within a new function readAndThen() and make it take as a parameter a callback function that is invoked only after the value.
 // The second step is determined by the actual function we feed into it as an argument when we invoke readFileAndThen()
+
+const taskExt = "TASK 1-extension >";
 
 // 1. We define the wrapped function
 function readFileAndThen(fileToRead, callback) {
   readFile(fileToRead, "utf-8", (err, data) => {
     if (err) {
-      console.log("TASK1 EXTENSION> error message >", err);
+      console.log(taskExt, errorX, err.message);
     } else {
-      console.log("TASK1 EXTENSION> ", data);
+      console.log(
+        taskExt,
+        success,
+        "Content length:",
+        data.length,
+        "characters"
+      );
+      console.log(taskExt, "Content Preview:", data.slice(0, 30) + "...");
       callback();
     }
   });
@@ -40,29 +62,14 @@ function readFileAndThen(fileToRead, callback) {
 
 // 2. We define what is the second step
 function printSuccess() {
-  console.log("Second Step!");
-}
-
-// 3. We invoke our new function
-readFileAndThen("./d", printSuccess);
-
-// Steps 2 and 3 can be rewritten with arrow functions as
-// readFileAndThen("./d", () => console.log("Second Step!"));
-
-//TASK 2
-const yourPromise = new Promise(executorFn);
-
-function executorFn(resolve, reject) {
-  return resolve(
-    "TASK2 > The promise has been fullfilled and the object promise is now resolved"
+  console.log(
+    taskExt,
+    ">>>>>>>>This function runs only after the file has been read!"
   );
 }
 
-yourPromise.then((message) => {
-  console.log(message);
-});
+// 3. We invoke our new function
+readFileAndThen("./data/02_poem.txt", printSuccess);
 
-// resolve("something") always returns undefined but acts as a switch by
-// 1. changing the value of the promise  from pendnig to resolved
-// 2. saving the message within the promise object
-// to extract the message (once available) and pass it to a function you use "then"
+// Steps 2 and 3 can be rewritten with arrow functions as
+// readFileAndThen("./d", () => console.log("Second Step!"));
