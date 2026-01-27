b699b9a873850bcf97bfb7ae0ea6c1defa1419f9
vertigo1919 <149095893+vertigo1919@users.noreply.github.com>
2026-01-27 16:22:36 +0000

    Finalised queries_join

--- northcoders/2_fundamentals/11-SQL1/db/queries/queries_JOIN.sql
+++ northcoders/2_fundamentals/11-SQL1/db/queries/queries_JOIN.sql
@@ -1,46 +1,63 @@
 -- Purpose: Learn JOIN
--- Notes: 
 
-\echo '\n >>> AUTHORS: the parent full table <<< \n'
+
+\echo '\n >>> Q1 - AUTHORS: the parent full table <<< \n'
 SELECT *
 FROM authors;
 
-\echo '\n >>> BOOKS: the child full table <<< \n' 
+
+\echo '\n >>> Q2 - BOOKS: the child full table <<< \n' 
 SELECT *
 FROM books;
 
-\echo '\n >>> Shows all books alongside their authors (filters out authors with zero books) using INNER JOIN <<< \n'
--- 'INNER JOIN: glues together rows from two tables that statisy the ON expression'
+
+\echo '\n >>> Q3 - Shows all books alongside their authors (filters out authors with zero books) <<< \n using INNER JOIN \n'
+-- INNER JOIN: glues together rows from two tables that statisy the ON expression
+
 SELECT  books.book_id,
         books.title,  
         authors.author_name
-
 FROM authors
     JOIN books ON authors.author_id = books.author_id;
 
-\echo '\n >>> Lists Authors wihout a book <<< \n (complement of the above, i.e. shows its exclusion set, using using LEFT OUTER JOIN)'
--- 'LEFT OUTER JOIN: keeps all rows of Table A, to which it glues rows from Table B if a match is found, or adds NULL values if not found'
-SELECT author_name, book_id
+
+\echo '\n >>> Q4 - Lists Authors wihout a book <<< \n (complement of Q3, i.e. shows its exclusion set,  using LEFT OUTER JOIN) \n'
+-- LEFT OUTER JOIN: keeps all rows of Table A, to which it glues rows from Table B if a match is found, or adds NULL values if not found
+SELECT author_name,
+       authors.author_id, 
+       book_id
 FROM authors
     LEFT OUTER JOIN books ON authors.author_id = books.author_id
 WHERE book_id IS null;
 
-\echo 'Authors wihout a book ("anti-join", i.e. everything that inner join excluded - using RIGHT OUTER JOIN)'
-SELECT author_name, book_id
+
+\echo '\n >>> Q5 - Lists authors wihout a book <<< \n (complement of Q3, i.e. shows its exclusion set, using  RIGHT OUTER JOIN) \n'
+-- RIGHT OUTER JOIN: keeps all rows of Table B, to which it glues rows from Table A if a match is found, or adds NULL values if not found
+
+SELECT author_name,
+       authors.author_id,
+       book_id
 FROM books
     RIGHT OUTER JOIN authors ON authors.author_id = books.author_id
 WHERE book_id IS null;
 
-\echo 'Authors wihout a book ("anti-join", i.e. everything that inner join excluded - using RIGHT OUTER JOIN)'
-SELECT author_name, book_id
-FROM books
-    RIGHT OUTER JOIN authors ON authors.author_id = books.author_id
-WHERE book_id IS null;
 
-\echo 'Generate every possible combination between the two table (cross join)'
+\echo '\n >>> Q6 - Lists books wihout an author (FULL OUTER JOIN) <<< \n '
+-- FULL OUTER JOIN: keeps all rows from both tables, if a match is found they are glue, if not NULL value are used for the table missing the match
+
+SELECT title,
+       author_name
+FROM books
+    FULL OUTER JOIN authors ON authors.author_id = books.author_id
+WHERE authors.author_id IS NULL AND books.book_id IS NOT NULL;
+
+
+\echo '\n >>> Q7 - Generate every possible combination between the two table (CROSS JOIN) <<< \n '
+-- CROSS JOIN
 SELECT authors.author_name, books.title
 FROM books
     CROSS JOIN authors
 LIMIT 10;
+
 
 
