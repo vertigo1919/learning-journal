4f227162d796a4e48aa8a12575f4fe0f80cf686e
vertigo1919 <149095893+vertigo1919@users.noreply.github.com>
2026-01-27 18:15:43 +0000

    Added junction table

--- northcoders/2_fundamentals/11-SQL1/db/schema.sql
+++ northcoders/2_fundamentals/11-SQL1/db/schema.sql
@@ -1,8 +1,10 @@
 -- Purpose: Defines the database schema for the bookshop domain
--- Notes: Some columns are added later using ALTER TABLE
+-- Notes: The order of operations is not by design, but based on learning patterns
 
-DROP TABLE IF EXISTS books;
-DROP TABLE IF EXISTS authors;
+-- 1) Implement idempotency
+DROP TABLE IF EXISTS books, authors, extra_books, genres;
+
+-- 2) CREATE books
 CREATE TABLE books (
     book_id SERIAL PRIMARY KEY,
     title VARCHAR(40) NOT NULL,
@@ -12,24 +14,44 @@ CREATE TABLE books (
     is_fiction BOOLEAN
 );
 
+-- 3) CREATE auhtors
 CREATE TABLE authors (
     author_id SERIAL PRIMARY KEY,
     author_name VARCHAR(40) NOT NULL,
     trivia  TEXT
 );
 
+-- 4) ALTER books to add a foregin key 
 ALTER TABLE books
 ADD author_id  INT,
-ADD CONSTRAINT fk_author
-FOREIGN KEY (author_id) 
-REFERENCES authors(author_id);
 
-create table extra_books(
+--we give the rule a human name
+ADD CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES authors(author_id);
+
+-- 5) CREATE extra_books
+CREATE TABLE extra_books(
     book_id serial primary key,
     title varchar,
     price numeric(5,2),
     quantity_in_stock int,
     release_date date,
     is_fiction boolean
+);
+
+-- 6) CREATE genres
+CREATE TABLE genres(
+    genre_id SERIAL PRIMARY KEY,
+    genre_name varchar
+);
+
+-- 7) CREATE book_genres junction table
+-- NB here we define the rules inline
+-- DELETE CASCADE is essential to guarante child (junction entry) to die on parent's death
+CREATE TABLE book_genres(
+    book_id INT REFERENCES books(book_id) ON DELETE CASCADE,
+    genre_id INT REFERENCES genres(genre_id) ON DELETE CASCADE,
+
+-- Here we define a composite key - essential to prevent multiple "tagging" e.g. assigning fiction 5 times to a novel
+    PRIMARY KEY (book_id, genre_id)
 );
 
--- northcoders/2_fundamentals/11-SQL1/db/seeds.sql
+++ northcoders/2_fundamentals/11-SQL1/db/seeds.sql
@@ -1,8 +1,12 @@
 -- Purpose: Inserts predictable seed data for local development
 -- Notes: dataset provided by Northcoders, intentionally small
 
-TRUNCATE TABLE books RESTART IDENTITY CASCADE;
-TRUNCATE TABLE authors RESTART IDENTITY CASCADE;
+-- 1) TRUNCATION
+/* The truncation below is not strictly necessary as tables are dropped by the schema
+ Leaving it here on the offchance this file is run on its own */
+TRUNCATE TABLE books, authors, genres RESTART IDENTITY CASCADE;
+
+-- 2) POPULATE BOOKS
 INSERT INTO books (
         title,
         price,
@@ -81,7 +85,7 @@ VALUES (
         'true'
     );
 
-
+-- 3) POPULATE AUTHORS
 INSERT INTO authors (
         author_name,
         trivia
@@ -149,13 +153,15 @@ VALUES (
     );
 
 
-/* Manual way
+-- 4) ASSIGN AUTHORS TO BOOKS
+
+/* 4A MANUALLY:
 UPDATE books
 SET author_id = 3
 WHERE title = 'The Hitchhiker''s Guide to the Galaxy'; 
 */
 
-/*USing CASE
+/* 4B Using CASE:
 UPDATE books
 SET author_id = CASE title
     WHEN 'The Hitchhiker''s Guide to the Galaxy' THEN 3
@@ -166,53 +172,184 @@ SET author_id = CASE title
 END
 WHERE title IN ('The Hitchhiker''s Guide to the Galaxy', 'The Little Prince', 'Emma', 'Nineteen Eighty-Four: A Novel');
 */
-
--- Using subqueries
-UPDATE books
-SET author_id = (SELECT author_id FROM authors WHERE author_name= 'Douglas Adams')
-WHERE title = 'The Hitchhiker''s Guide to the Galaxy'; 
+-- 4C Using SUBQUERIES: 
 
 UPDATE books
-SET author_id = (SELECT author_id FROM authors WHERE author_name= 'Antoine de Saint-Exupéry')
-WHERE title = 'The Little Prince'; 
-
+SET author_id = (
+        SELECT author_id
+        FROM authors
+        WHERE author_name = 'Douglas Adams'
+    )
+WHERE title = 'The Hitchhiker''s Guide to the Galaxy';
 UPDATE books
-SET author_id = (SELECT author_id FROM authors WHERE author_name= 'Beatrix Potter')
-WHERE title = 'The Tale of Peter Rabbit'; 
-
+SET author_id = (
+        SELECT author_id
+        FROM authors
+        WHERE author_name = 'Antoine de Saint-Exupéry'
+    )
+WHERE title = 'The Little Prince';
 UPDATE books
-SET author_id = (SELECT author_id FROM authors WHERE author_name= 'Jane Austen')
-WHERE title = 'Emma'; 
-
+SET author_id = (
+        SELECT author_id
+        FROM authors
+        WHERE author_name = 'Beatrix Potter'
+    )
+WHERE title = 'The Tale of Peter Rabbit';
 UPDATE books
-SET author_id = (SELECT author_id FROM authors WHERE author_name= 'George Orwell')
-WHERE title = 'Nineteen Eighty-Four: A Novel'; 
-
+SET author_id = (
+        SELECT author_id
+        FROM authors
+        WHERE author_name = 'Jane Austen'
+    )
+WHERE title = 'Emma';
 UPDATE books
-SET author_id = (SELECT author_id FROM authors WHERE author_name= 'Margaret Atwood')
-WHERE title = 'The Handmaid''s Tale'; 
-
+SET author_id = (
+        SELECT author_id
+        FROM authors
+        WHERE author_name = 'George Orwell'
+    )
+WHERE title = 'Nineteen Eighty-Four: A Novel';
 UPDATE books
-SET author_id = (SELECT author_id FROM authors WHERE author_name= 'H. G. Wells')
-WHERE title = 'The War of the Worlds'; 
-
+SET author_id = (
+        SELECT author_id
+        FROM authors
+        WHERE author_name = 'Margaret Atwood'
+    )
+WHERE title = 'The Handmaid''s Tale';
 UPDATE books
-SET author_id = (SELECT author_id FROM authors WHERE author_name= 'Jane Austen')
-WHERE title = 'Pride and Prejudice'; 
-
+SET author_id = (
+        SELECT author_id
+        FROM authors
+        WHERE author_name = 'H. G. Wells'
+    )
+WHERE title = 'The War of the Worlds';
 UPDATE books
-SET author_id = (SELECT author_id FROM authors WHERE author_name= 'Louis de Bernières')
-WHERE title = 'Captain Corelli''s Mandolin'; 
-
+SET author_id = (
+        SELECT author_id
+        FROM authors
+        WHERE author_name = 'Jane Austen'
+    )
+WHERE title = 'Pride and Prejudice';
 UPDATE books
-SET author_id = (SELECT author_id FROM authors WHERE author_name= 'Stephen Hawking')
-WHERE title = 'A Brief History of Time'; 
+SET author_id = (
+        SELECT author_id
+        FROM authors
+        WHERE author_name = 'Louis de Bernières'
+    )
+WHERE title = 'Captain Corelli''s Mandolin';
+UPDATE books
+SET author_id = (
+        SELECT author_id
+        FROM authors
+        WHERE author_name = 'Stephen Hawking'
+    )
+WHERE title = 'A Brief History of Time';
 
-insert into extra_books(title, price, quantity_in_stock, release_date, is_fiction)
-values
-('The Hitchhiker''s Guide to the Galaxy', 8.99, 560,'1997-10-12',true),
-('The Little Prince', 6.99, 1020, '1943-04-06',true),
-('Pride and Prejudice',6.99,4,'1813-01-28',true),
-('Pride and Prejudice',6.99,12,'1813-01-28',true),
-('Pride and Prejudice',6.99,2,'1813-01-28',true),
-('Pride and Prejudice',6.99,8,'1813-01-28',true);
+-- 5) POPULATE EXTRA_BOOKS
+INSERT INTO extra_books(
+        title,
+        price,
+        quantity_in_stock,
+        release_date,
+        is_fiction
+    ) VALUES (
+        'The Hitchhiker''s Guide to the Galaxy',
+        8.99,
+        560,
+        '1997-10-12',
+        true
+    ),
+    (
+        'The Little Prince',
+        6.99,
+        1020,
+        '1943-04-06',
+        true
+    ),
+    (
+        'Pride and Prejudice',
+        6.99,
+        4,
+        '1813-01-28',
+        true
+    ),
+    (
+        'Pride and Prejudice',
+        6.99,
+        12,
+        '1813-01-28',
+        true
+    ),
+    (
+        'Pride and Prejudice',
+        6.99,
+        2,
+        '1813-01-28',
+        true
+    ),
+    (
+        'Pride and Prejudice',
+        6.99,
+        8,
+        '1813-01-28',
+        true
+    );
+
+
+-- 5) POPULATE EXTRA_BOOKS
+
+INSERT INTO genres (genre_name) VALUES ('science fiction'), ('children''s'), ('romance'), ('fantasy'), ('dystopian'), ('science'), ('adventure'), ('classics');
+
+-- 6) POPULATE JUNCTION TABLE book_genres
+
+INSERT INTO book_genres (book_id, genre_id)
+VALUES (1, 1),
+    -- Hitchhiker's Guide -> Science Fiction
+    (1, 7),
+    -- Hitchhiker's Guide -> Adventure
+    (1, 8),
+    -- Hitchhiker's Guide -> Classics
+    (2, 2),
+    -- The Little Prince -> Children's
+    (2, 4),
+    -- The Little Prince -> Fantasy
+    (2, 8),
+    -- The Little Prince -> Classics
+    (3, 2),
+    -- Peter Rabbit -> Children's
+    (3, 8),
+    -- Peter Rabbit -> Classics
+    (4, 3),
+    -- Emma -> Romance
+    (4, 8),
+    -- Emma -> Classics
+    (5, 1),
+    -- 1984 -> Science Fiction
+    (5, 5),
+    -- 1984 -> Dystopian
+    (5, 8),
+    -- 1984 -> Classics
+    (6, 1),
+    -- Handmaid's Tale -> Science Fiction
+    (6, 5),
+    -- Handmaid's Tale -> Dystopian
+    (6, 8),
+    -- Handmaid's Tale -> Classics
+    (7, 1),
+    -- War of the Worlds -> Science Fiction
+    (7, 7),
+    -- War of the Worlds -> Adventure
+    (7, 8),
+    -- War of the Worlds -> Classics
+    (10, 3),
+    -- Pride & Prejudice -> Romance
+    (10, 8),
+    -- Pride & Prejudice -> Classics
+    (8, 3),
+    -- Captain Corelli's Mandolin -> Romance
+    (8, 8),
+    -- Captain Corelli's Mandolin -> Classics
+    (9, 6),
+    -- Brief History of Time -> Science
+    (9, 8);
+    -- Brief History of Time -> Classics;
