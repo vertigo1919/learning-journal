211f974d793c3921435a2c2cb9150ff1cf6cf0dd
vertigo1919 <149095893+vertigo1919@users.noreply.github.com>
2026-02-16 19:54:52 +0000

    fixed demployment for netifly

--- northcoders/4_frontend/2-React/jukebox/vite.config.js
+++ northcoders/4_frontend/2-React/jukebox/vite.config.js
@@ -4,16 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/learning-journal/jukebox/', 
-
-  build: {
-    // 2. SET THE OUTPUT DIRECTORY
-    // We use a relative path to go UP 4 levels to the repo root, 
-    // then into a "docs" folder.
-    outDir: '../../../../docs/jukebox',
-    
-    // 3. CLEANUP
-    // This clears the folder before building so old files don't pile up
-    emptyOutDir: true, 
-  }
+  base: "/learning-journal/jukebox/",
 });
