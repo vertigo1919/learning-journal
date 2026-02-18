fad62ed018da0a932cceac15c2811877c3b40e06
vertigo1919 <149095893+vertigo1919@users.noreply.github.com>
2026-02-18 13:53:19 +0000

    Cleanup

--- northcoders/4_frontend/2-React/2-data-fetching-in-react/src/components/UserList.jsx
+++ northcoders/4_frontend/2-React/2-data-fetching-in-react/src/components/UserList.jsx
@@ -5,16 +5,13 @@ function UserList() {
   const [users, setUsers] = useState({});
 
   // TASK 01 > run some code when the component mounts
-  useEffect(() => console.log("This is only runs once"), []);
+  useEffect(() => console.log("This only runs once"), []);
 
-  // TASK 02 and 03 > Fetch user and store in state
-
+  // TASK 02 and 03 > Fetch user and store in a state
   async function fetchUser(userID) {
-    //if (users.length > 5){return}else{
     const user = await getUserById(userID);
 
     setUsers(user);
-    //}
   }
 
   useEffect(() => {
@@ -26,37 +23,3 @@ function UserList() {
 }
 
 export default UserList;
-
-// useEffect ((fetchUser(3) , []))
-// useEffect(()=> fetchUser(3), [])
-
-// It looks like you wrote useEffect(async () => ...) or returned a Promise. Instead, write the async function inside your effect and call it immediately:
-
-// useEffect(() => {
-//   async function fetchData() {
-//     // You can await here
-//     const response = await MyAPI.getData(someId);
-//     // ...
-//   }
-//   fetchData();
-// }, [someId]); // Or [] if effect doesn't need props or state
-
-// setUsers([...users , user])
-//    console.log(">>", users)
-
-// OLD
-//// TASK 01
-// const [testlog , setTestLog] = useState("")
-// function testLog ()
-// {console.log("test")}
-
-// TASK 01 v2
-//useEffect (()=> console.log("This is only runs once") , [])
-
-///// TASK 02 /////
-// async function fetchUser(userID) {
-//   const user = await getUserById(userID)
-//   console.log(user)
-// }
-// useEffect ((fetchUser(3) , []))
-////////////
